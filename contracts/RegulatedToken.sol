84f944df748db5c08d68d029c793f3c546d1748f
0v1se <dev@0v1se.com>
2018-05-10 16:39:18 +0300

    doc

--- contracts/RegulatedToken.sol
+++ contracts/RegulatedToken.sol
@@ -3,5 +3,8 @@ pragma solidity ^0.4.23;
 import "@daonomic/interfaces/contracts/MintableToken.sol";
 
 contract RegulatedToken is MintableToken {
+    /**
+     * @dev Check if investor is able to receive token
+     */
     function ableToReceive(address _address, uint256 _amount) constant public returns (bool);
 }
