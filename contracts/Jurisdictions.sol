9e4c52a3ae5abfbc217adc51cf67df3cc5cefe5f
0v1se <dev@0v1se.com>
2018-06-07 17:17:58 +0300

    0.1.2 public constants in Jurisdictions  + added simple tests

--- contracts/Jurisdictions.sol
+++ contracts/Jurisdictions.sol
@@ -5,7 +5,7 @@ pragma solidity ^0.4.23;
  * @dev Enumerates known jurisdictions
  */
 contract Jurisdictions {
-    uint16 constant US = 1;
-    uint16 constant ALLOWED = 2**16 -2;
-    uint16 constant OTHER = 2**16 - 1;
+    uint16 public constant US = 1;
+    uint16 public constant ALLOWED = 2**16 - 2;
+    uint16 public constant OTHER = 2**16 - 1;
 }
---
+++ test/RegulatorServiceImpl.js
@@ -0,0 +1,36 @@
+const AllowRule = artifacts.require('AllowRegulationRule.sol');
+const DenyRule = artifacts.require('DenyRegulationRule.sol');
+const RegulatorServiceImpl = artifacts.require('RegulatorServiceImpl.sol');
+const KycProviderImpl = artifacts.require('KycProviderImpl.sol');
+const Token = artifacts.require('TestRegulatedToken.sol');
+
+const tests = require("@daonomic/tests-common");
+const awaitEvent = tests.awaitEvent;
+const expectThrow = tests.expectThrow;
+const randomAddress = tests.randomAddress;
+const randomInt = tests.randomInt;
+
+contract("RegulatorServiceImpl", accounts => {
+	let token;
+	let testing;
+	let allow;
+	let kyc;
+
+	beforeEach(async () => {
+		testing = await RegulatorServiceImpl.new();
+		token = await Token.new(testing.address);
+		allow = await AllowRule.new();
+		kyc = await KycProviderImpl.new();
+	});
+
+	it("should return true if allowed by Rule", async () => {
+		await testing.setKycProviders(token.address, [kyc.address]);
+		await testing.setRule(token.address, 2 ^ 16 - 2, allow.address);
+
+		var address = randomAddress();
+		await kyc.setData(address, 2 ^ 16 - 2, "");
+		assert.equal(await testing.canMint(address, 1, {from: token.address}), true);
+		assert.equal(await testing.canMint(address, 1, {from: randomAddress()}), false);
+		assert.equal(await testing.canMint(randomAddress(), 1, {from: token.address}), false);
+	});
+});
