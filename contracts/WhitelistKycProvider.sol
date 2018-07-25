eef3486da845706e3413430c82477717eb93e99a
0v1se <dev@0v1se.com>
2018-07-25 11:27:48 +0300

    0.1.12 - Whitelist Kyc Provider adds operator role

--- contracts/WhitelistKycProvider.sol
+++ contracts/WhitelistKycProvider.sol
@@ -5,10 +5,11 @@ pragma experimental ABIEncoderV2;
 import "@daonomic/util/contracts/OwnableImpl.sol";
 import "@daonomic/interfaces/contracts/KycProvider.sol";
 import "@daonomic/interfaces/contracts/Whitelist.sol";
+import "@daonomic/util/contracts/SecuredImpl.sol";
 import "./Jurisdictions.sol";
 
 
-contract WhitelistKycProvider is Jurisdictions, OwnableImpl, Whitelist, KycProvider {
+contract WhitelistKycProvider is Jurisdictions, OwnableImpl, SecuredImpl, Whitelist, KycProvider {
   mapping(address => bool) whitelist;
 
   function resolve(address _address) constant public returns (Investor) {
@@ -23,19 +24,19 @@ contract WhitelistKycProvider is Jurisdictions, OwnableImpl, Whitelist, KycProvi
     return whitelist[addr];
   }
 
-  function addToWhitelist(address[] _addresses) onlyOwner public {
+  function addToWhitelist(address[] _addresses) ownerOr("operator") public {
     for (uint i = 0; i < _addresses.length; i++) {
       setWhitelistInternal(_addresses[i], true);
     }
   }
 
-  function removeFromWhitelist(address[] _addresses) onlyOwner public {
+  function removeFromWhitelist(address[] _addresses) ownerOr("operator") public {
     for (uint i = 0; i < _addresses.length; i++) {
       setWhitelistInternal(_addresses[i], false);
     }
   }
 
-  function setWhitelist(address addr, bool allow) onlyOwner public {
+  function setWhitelist(address addr, bool allow) ownerOr("operator") public {
     setWhitelistInternal(addr, allow);
   }
 
